51a6dbf9d0a7edf4b534dd22d2cb99a02b72555b
Arzo Thakur <thakur.arzo@yahoo.comm>
2019-07-31 16:07:07 -0500

    foodow front-end setup

---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+
+<template>
+ <div id="menuItems">
+  <div v-for="(menuItems, index) in menuItems" :key="menuItems.id">
+  </div>
+ </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+ data() {
+  return {
+   menuItems: [],
+   title: [],
+   chain: [],
+   };
+  },
+ 
+ created: function() {
+   console.log("i am in created");
+   axios.get("/api/index").then(response => {
+     console.log(response.data);
+     this.menuItems = response.data;
+   });
+ },
+ // .catch(e => {
+ //  this.error.push(e)
+ //  })
+ }
+
+</script>
